ac4470d573985b6be7ed9cff39407395a361cc93
mmorales68 <miguelm4032@gmail.com>
2024-03-02 10:23:35 -0600

    ejercicio 2.2 realizado por - Miguel Enrique Morales Martinez 0909-23-14463

--- parcial1.cpp
+++ parcial1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
+    /*
     double a=0,b=0;
     for (size_t i = 0; i <= 50; i++){
         if (i%2==0){
@@ -8,6 +9,11 @@ int main(){
         }else{
             a=a+i;
         }
+    }*/
+
+    int i=0;
+    while (i%2 != 1 || i <= 30){
+        i=i+3;
     }
     return 0;
 }
